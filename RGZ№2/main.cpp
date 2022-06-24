667196b8ad9feda808fd3fc37da68b75259196c3
dmitryyahno <dmitry.yahno1@gmail.com>
2022-06-24 17:44:13 +0300

    adding Header function

--- RGZ№2/main.cpp
+++ RGZ№2/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include "struct.h"
 #include "reading.h"
+#include "writing.h"
 
 using namespace std;
 bool checkmp3(string filename);
@@ -13,5 +14,16 @@ int main()
 	return(0);
 }
 
-
+string Header(FILE* file, int readloc)
+{
+	//Checks for ID3v1 Tag Header 4bytes long...
+	//currently only supports v1
+	//v1+ coming later
+	char magic[4];
+	fseek(file, readloc, SEEK_SET);
+	fread(magic, 1, 4, file);
+	string str(magic);
+	string mag = str.substr(0, 3);
+	return mag;
+}
 
