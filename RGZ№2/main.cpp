c7614f7e2e67c8264fdf63968d0704ea205c4ad2
MaximBoluchevskyi <bnm260811@gmail.com>
2022-06-24 15:42:43 +0300

    First commit
    Creating project

---
+++ RGZ№2/main.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "struct.h"
+
+using namespace std;
+bool checkmp3(string filename);
+string Header(FILE* file, int readloc);
+int main()
+{
+	return(0);
+}
+
+
+
---
+++ RGZ№2/struct.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<string>
+using namespace std;
+struct MP3Tag
+{
+	string Artist;
+	string Title;
+	string Album;
+	string Year;
+	string Comment;
+};
+
